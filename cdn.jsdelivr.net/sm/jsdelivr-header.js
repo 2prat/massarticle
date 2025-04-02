0dfab55308fe3c459cfb8164f71b8e6e7f390fe9
Prathamesh <138348599+2prat@users.noreply.github.com>
2025-04-03 00:25:36 +0530

    Add files via upload

---
+++ cdn.jsdelivr.net/sm/jsdelivr-header.js
@@ -0,0 +1,6 @@
+/**
+ * Minified by jsDelivr using Terser v5.37.0.
+ * Original file: /npm/timeago@1.6.7/jquery.timeago.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
